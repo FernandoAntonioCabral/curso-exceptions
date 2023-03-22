6d73bd272e2c3ffaef75bf4d34992faf62ea966a
Fernando Cabral <fernandoantoniojp10@gmail.com>
2023-03-22 20:06:54 -0300

    Criado projeto basico de uma conta para teste de Exceptions

---
+++ Curso/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string massage) :base(massage) 
+        { 
+
+        }   
+        
+    }
+}
---
+++ Curso/Program.cs
@@ -0,0 +1,38 @@
+﻿using Curso.Entities;
+using System;
+using Curso.Entities.Exceptions;
+using System.Globalization;
+
+namespace Curso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double wdLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, wdLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(amount);
+
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            } catch (DomainException e) 
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
